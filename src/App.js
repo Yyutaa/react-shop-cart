0f9ea1e2696b3d572d5ba918c7941a492eea0dd4
yuta <yangguangben@houselai.com>
2021-04-12 16:37:37 +0800

    项目基本配置

---
+++ src/App.js
@@ -0,0 +1,31 @@
+/*
+ * @Author: yuta
+ * @Date: 2021-04-12 16:23:16
+ * @LastEditTime: 2021-04-12 16:36:31
+ * @LastEditors: yuta
+ */
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
